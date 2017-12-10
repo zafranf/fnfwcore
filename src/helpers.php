c71e2970f602a025313f289c05c603f9b42b441f
zafranf <zafranfadilah@gmail.com>
2017-12-10 19:40:38 +0700

    pindah beberapa function

--- src/helpers.php
+++ src/helpers.php
@@ -63,3 +63,152 @@ if (!function_exists('generateFlashMessages')) {
         }
     }
 }
+
+/**
+ * [config description]
+ * @return [type] [description]
+ */
+if (!function_exists('config')) {
+    function config($key = null)
+    {
+        $conf = require ROOT_PATH . 'vendor/zafranf/fnfwcore/src/Config/config.php';
+
+        if (!is_null($key) && isset($conf[$key])) {
+            $conf = $conf[$key];
+        }
+
+        return $conf;
+    }
+}
+
+/**
+ * [debug description]
+ * @param  [type]  $data [description]
+ * @param  boolean $die  [description]
+ * @return [type]        [description]
+ */
+if (!function_exists('debug')) {
+    function debug()
+    {
+        array_map(function ($data) {
+            echo "<pre>";
+            print_r($data);
+            echo "</pre>";
+        }, func_get_args());
+
+        die();
+    }
+}
+
+/**
+ * [nf description]
+ * @param  [type] $num [description]
+ * @return [type]      [description]
+ */
+if (!function_exists('nf')) {
+    function nf($num, $digit = 0, $coms = ",", $dots = ".")
+    {
+        return number_format($num, $digit, $coms, $dots);
+    }
+}
+
+/**
+ * [slug description]
+ * @param  [type] $text [description]
+ * @param  string $rep  [description]
+ * @return [type]       [description]
+ */
+if (!function_exists('slug')) {
+    function slug($text, $rep = "-")
+    {
+        $text = strtolower($text);
+        $text = preg_replace('([\s\W\_]+)', $rep, $text);
+
+        return $text;
+    }
+}
+
+/**
+ * [url description]
+ * @param  string  $url  [description]
+ * @param  boolean $full [description]
+ * @return [type]        [description]
+ */
+if (!function_exists('url')) {
+    function url($url = "", $full = false)
+    {
+        if (filter_var($url, FILTER_VALIDATE_URL)) {
+            return $url;
+        }
+        if ($full) {
+            $urls = explode("?", $_SERVER['REQUEST_URI']);
+            $segment = $urls[0];
+        }
+        return sprintf(
+            "%s://%s%s%s",
+            isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+            $_SERVER['SERVER_NAME'] . '/',
+            isset($segment) ? ltrim($segment, '/') : '',
+            $url != "/" ? ltrim($url, '/') : ''
+        );
+    }
+}
+
+/**
+ * [bool description]
+ * @param  [type]  $str [description]
+ * @return [type]        [description]
+ */
+if (!function_exists('bool')) {
+    function bool($str = false)
+    {
+        if (is_string($str) || is_int($str)) {
+            $str = strtolower(trim($str));
+            if ($str == 'true' || $str == 't' || $str == 'yes' || $str == 'y' || $str == '1') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+/**
+ * [is_json description]
+ * @param  [type]  $data [description]
+ * @return [type]        [description]
+ */
+if (!function_exists('is_json')) {
+    function is_json($data = null)
+    {
+        if (!is_null($data)) {
+            @json_decode($data);
+            return (json_last_error() === JSON_ERROR_NONE);
+        }
+
+        return false;
+    }
+}
+
+/**
+ * [auth description]
+ * @return boolean [description]
+ */
+if (!function_exists('auth')) {
+    function auth($key = '')
+    {
+        $auth = [];
+        if (isLogin()) {
+            $auth = _session('user');
+            if ($key != '') {
+                if (isset($auth[$key])) {
+                    $auth = $auth[$key];
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        return $auth;
+    }
+}
